2bb60916919b4c4a45d27e3ffada868163666f9e
AsusMiguel <migalvez2002@gmail.com>
2024-10-28 22:06:22 -0500

    commit

--- src/main/java/com/miguel/springboot/suicidedetection/suicidedetection/config/GateConfig.java
+++ src/main/java/com/miguel/springboot/suicidedetection/suicidedetection/config/GateConfig.java
@@ -2,19 +2,23 @@ package com.miguel.springboot.suicidedetection.suicidedetection.config;
 
 import gate.Factory;
 import gate.Gate;
+import gate.LanguageAnalyser;
+import gate.ProcessingResource;
 import gate.creole.Plugin;
 import gate.creole.SerialAnalyserController;
 import gate.util.GateException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Arrays;
 
 @Configuration
 public class GateConfig {
 
     @Bean
-    public SerialAnalyserController serialAnalyserController() throws GateException {
+    public SerialAnalyserController serialAnalyserController() throws GateException, MalformedURLException {
         // Inicializar GATE
         Gate.init();
 
@@ -22,6 +26,10 @@ public class GateConfig {
         Plugin anniePlugin = new Plugin.Maven(
                 "uk.ac.gate.plugins", "annie", gate.Main.version);
         Gate.getCreoleRegister().registerPlugin(anniePlugin);
+
+        Plugin learningPlugin = new Plugin.Maven(
+                "uk.ac.gate.plugins", "learningframework", "4.2");
+        Gate.getCreoleRegister().registerPlugin(learningPlugin);
 
         // create a serial analyser controller to run ANNIE with
         SerialAnalyserController annieController =
@@ -35,9 +43,20 @@ public class GateConfig {
                 "gate.creole.splitter.SentenceSplitter",
                 "gate.creole.POSTagger",
                 "gate.creole.ANNIETransducer",
-                "gate.creole.orthomatcher.OrthoMatcher")) {
+                "gate.creole.orthomatcher.OrthoMatcher"
+                )) {
             annieController.add((gate.LanguageAnalyser) Factory.createResource(pr));
         }
+
+        ProcessingResource evaluatePR = (ProcessingResource) Factory.createResource(
+                "gate.plugin.learningframework.LF_ApplyClassification");
+
+        //evaluatePR.setParameterValue("dataDirectory", "file:/C:/Users/magq2/Documents/proyectoGradoTwitter/datos/trainedModel");
+        //evaluatePR.setParameterValue("instanceType", "Key");
+        //evaluatePR.setParameterValue("outputASName", "Learning");
+        //evaluatePR.setParameterValue("modelURL", new URL("file:/C:/Users/magq2/Documents/proyectoGradoTwitter/datos/trainedModel/lf.model"));
+
+        //annieController.add(evaluatePR);
         return annieController;
     }
 
